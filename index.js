b3e54f205beffa0e5ba1a1712125e0a3b2874c5e
Solomon Gilala <75948157+GiloGilala@users.noreply.github.com>
2022-08-24 13:31:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(8800, () => {
-  connect();
-  console.log("Connected to Server");
-});
+const port = process.env.PORT || 4000
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
