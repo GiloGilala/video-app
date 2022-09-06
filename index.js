a417a1871e04df6427678ccecdd0196dee832364
Solomon Gilala <75948157+GiloGilala@users.noreply.github.com>
2022-09-06 17:20:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import videoRoutes from "./routes/videos.js";
 import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 
 
 
@@ -25,7 +27,8 @@ mongoose.connection.on("disconnected", () => {
 });
 
 //middlewares
-app.use(cookieParser())
+app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
