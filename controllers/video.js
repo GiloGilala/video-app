19260ec96c319cc40d753b6d5adbef7917e2687e
Solomon Gilala <75948157+GiloGilala@users.noreply.github.com>
2022-09-22 16:52:05 +0100

    Update video.js

--- controllers/video.js
+++ controllers/video.js
@@ -3,7 +3,7 @@ import Video from "../models/Video.js";
 import { createError } from "../error.js";
 
 export const addVideo = async (req, res, next) => {
-  const newVideo = new Video({ userId: req.user.id, ...req.body });
+  const newVideo = new Video({ ...req.body });
   try {
     const savedVideo = await newVideo.save();
     res.status(200).json(savedVideo);
@@ -16,7 +16,7 @@ export const updateVideo = async (req, res, next) => {
   try {
     const video = await Video.findById(req.params.id);
     if (!video) return next(createError(404, "Video not found!"));
-    if (req.user.id === video.userId) {
+    
       const updatedVideo = await Video.findByIdAndUpdate(
         req.params.id,
         {
@@ -25,9 +25,7 @@ export const updateVideo = async (req, res, next) => {
         { new: true }
       );
       res.status(200).json(updatedVideo);
-    } else {
-      return next(createError(403, "You can update only your video!"));
-    }
+    
   } catch (err) {
     next(err);
   }
